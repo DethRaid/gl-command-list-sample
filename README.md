4bffc1f5320d28fd1dee4d9f2a5feaa8f5dcf0d4
David Dubois <dexcelstraun7@gmail.com>
2020-06-13 00:17:28 -0700

    Added basic vertex shader and a marginally useful README

---
+++ README.md
@@ -0,0 +1,6 @@
+# NV_GL_command_list LWJGL sample
+
+Sample LWJGL application showing how one might use the NV_GL_command_list extension
+
+This application uses the extensions `NV_GL_command_list` and `ARB_GL_bindless_texture`. Additionally, it targets OpenGL 
+4.5 and Java 1.8. If your system doesn't support those things, tough.
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,6 +1,7 @@
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.NVCommandList.*;
+import static org.lwjgl.opengl.GL46C.*;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -8,6 +9,7 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Driver {
     private final long window;
+    private int standardProgram;
 
     public static void main(String[] args) {
         Driver driver = new Driver();
@@ -46,7 +48,46 @@ public class Driver {
     }
 
     private void loadShaders() {
+        final String vertexShaderSource = "#version 450\n" +
+                "" +
+                "#extension GL_NV_command_list : enable\n" +
+                "" +
+                "layout(location = 0) in vec3 position_ia;" +
+                "layout(location = 1) in vec2 texcoord_ia;" +
+                "" +
+                "layout(std140, binding=0) uniform scene_data {" +
+                "    mat4 view_projection_matrix;" +
+                "}" +
+                "" +
+                "layout(std140, binding=1) uniform object_data {" +
+                "    mat4 model_matrix;" +
+                "    sampler2D color_texture;" +
+                "}" +
+                "" +
+                "layout(location = 0) out vec2 texcoord_vs;" +
+                "" +
+                "void main() {" +
+                "    gl_Position = scene_data.view_projection_matrix * object_data.model_matrix * vec4(position_ia, 1);" +
+                "    texvoord_vs = texcoord_ia;" +
+                "}";
 
+        final String fragmentShaderSource = "";
+
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, vertexShaderSource);
+        glCompileShader(vertexShader);
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, fragmentShaderSource);
+        glCompileShader(fragmentShader);
+
+        standardProgram = glCreateProgram();
+
+        glAttachShader(standardProgram, vertexShader);
+        glAttachShader(standardProgram, fragmentShader);
+        glLinkProgram(standardProgram);
+
+        // Assume everything worked fine cause I'm the best
     }
 
     private void loop() {
